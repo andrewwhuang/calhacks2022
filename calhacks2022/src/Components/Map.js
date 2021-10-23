45e35bd38d8a59e6eddef3f4f105148d9dd2d498
Andrew Huang <andrewhuang0917@yahoo.com>
2021-10-23 14:00:39 -0700

    Refactored map

--- calhacks2022/src/Components/Map.js
+++ calhacks2022/src/Components/Map.js
@@ -27,25 +27,20 @@ function Map(props) {
     Geocode.fromLatLng(coords.lat, coords.lng).then(
       (response) => {
         let city, state, country;
-        for (
-          let i = 0;
-          i < response.results[0].address_components.length;
-          i++
-        ) {
-          for (
-            let j = 0;
-            j < response.results[0].address_components[i].types.length;
-            j++
-          ) {
-            switch (response.results[0].address_components[i].types[j]) {
+        let resultObject = response.results[0];
+        for (let i = 0; i < resultObject.address_components.length; i++) {
+            const currObj = resultObject.address_components[i];
+          for (let j = 0; j < currObj.types.length; j++) {
+              // is short name needed instead of long name?
+            switch (currObj.types[j]) {
               case "locality":
-                city = response.results[0].address_components[i].long_name;
+                city = currObj.long_name;
                 break;
               case "administrative_area_level_1":
-                state = response.results[0].address_components[i].long_name;
+                state = currObj.long_name;
                 break;
               case "country":
-                country = response.results[0].address_components[i].long_name;
+                country = currObj.long_name;
                 break;
               default:
                 break;
