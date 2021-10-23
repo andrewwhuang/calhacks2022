d6af22c48242332fe279c0ad66dae5c46c5f10aa
matthewkim224 <73864058+matthewkim224@users.noreply.github.com>
2021-10-22 22:58:39 -0700

    fixed react webhook error

--- calhacks2022/src/Components/Map.js
+++ calhacks2022/src/Components/Map.js
@@ -6,43 +6,41 @@ import {
   InfoWindow,
 } from "@react-google-maps/api";
 
-class Map extends Component {
-  getNearestCity() {
+function Map(props) {
+  function getNearestCity() {
     return "apples";
   }
 
-  render() {
-    const libraries = ["places"];
+  const libraries = ["places"];
 
-    const { isLoaded, loadError } = useLoadScript({
-      googleMapsApiKey: "AIzaSyCLdIa6-k1T6ezYBWLxa6o1NfukLxggROE",
-      libraries: libraries,
-    });
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCLdIa6-k1T6ezYBWLxa6o1NfukLxggROE",
+    libraries: libraries,
+  });
 
-    if (loadError) return "Error Loading Map";
-    if (!isLoaded) return "Loading Maps";
+  if (loadError) return "Error Loading Map";
+  if (!isLoaded) return "Loading Maps";
 
-    const mapContainerStyle = {
-      width: "500px",
-      height: "500px",
-    };
+  const mapContainerStyle = {
+    width: "500px",
+    height: "500px",
+  };
 
-    if (this.props.data) {
-      var center = { lat: this.props.data.lat, lng: this.props.data.lng };
-    }
-
-    return (
-      <section id="map">
-        {" "}
-        "sample test for map" + {this.getNearestCity()}
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          zoom={8}
-          center={center}
-        ></GoogleMap>
-      </section>
-    );
+  if (props.data) {
+    var center = { lat: props.data.lat, lng: props.data.lng };
   }
+
+  return (
+    <section id="map">
+      {" "}
+      "sample test for map" + {getNearestCity()}
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+      ></GoogleMap>
+    </section>
+  );
 }
 
 export default Map;
