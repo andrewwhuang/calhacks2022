47a96fab634fa70a273edbc47dd263c82e17f1ca
Andrew Huang <andrewhuang0917@yahoo.com>
2021-10-23 17:18:14 -0700

    a

--- calhacks2022/src/Components/PreferencesBar.js
+++ calhacks2022/src/Components/PreferencesBar.js
@@ -28,7 +28,6 @@ export default class PreferencesBar extends Component {
 
             <Menu.Menu position='right'>
                 <Menu.Item>
-                    <label className="labelName">Start date </label>
                     <form onSubmit={ this.onFormSubmit }>
                         <DatePicker
                             selected={ this.state.startDate }
@@ -41,12 +40,12 @@ export default class PreferencesBar extends Component {
                             name="startDate"
                             dateFormat="yyyy-MM-dd"
                             wrapperClassName="datePicker-form"
+                            placeholderText='Start Date'
                         />
                     </form>
                 </Menu.Item>
             <Menu.Item>
             <Menu.Item>
-                    <label className="labelName">End date </label>
                     <form onSubmit={ this.onFormSubmit }>
                         <DatePicker
                             selected={ this.state.endDate }
@@ -59,14 +58,14 @@ export default class PreferencesBar extends Component {
                             name="endDate"
                             dateFormat="yyyy-MM-dd"
                             wrapperClassName="datePicker-form"
+                            placeholderText='End Date'
                         />
                     </form>
                 </Menu.Item>
             </Menu.Item>
             <Menu.Item>
                 <Form.Field>
-                    <label className="labelName">Start location </label>
-                    <Input placeholder='Start location' />
+                    <Input placeholder='Enter City' />
                 </Form.Field>
             </Menu.Item>
             <Menu.Item>
@@ -77,7 +76,7 @@ export default class PreferencesBar extends Component {
                     className="Btn-BG"
                 >
                     <ToggleButton value="CAD">Canadian Dollar</ToggleButton>
-                    <ToggleButton value="USD">US Dollar</ToggleButton>
+                    <ToggleButton value="USD"> {"   US Dollar   "} </ToggleButton>
                 </ToggleButtonGroup>
             </Menu.Item>
 
